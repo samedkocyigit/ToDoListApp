7d54b89b95cd1a70ed4d8a9202d5c028cb20dbeb
samedkocyigit <abdulsamed.kcygt@gmail.com>
2024-10-04 01:58:07 +0300

    Dto's has been created for data security

---
+++ ToDoListApp/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListApp.DTOs
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+
+}
---
+++ ToDoListApp/DTOs/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoListApp.DTOs
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
