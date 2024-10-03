f69fbecd0b598ea7a62e225f779d3611e9ddc5d3
samedkocyigit <abdulsamed.kcygt@gmail.com>
2024-10-04 01:59:45 +0300

    Jwt and Configurations file has been arranged

--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -4,6 +4,11 @@ using static ToDoListApp.Data.Repositories.Abstract.IGenericRepository;
 using ToDoListApp.Data.Repositories.Concrete;
 using ToDoListApp.Data.Repositories.Abstract;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ToDoListApp.Services.Abstract;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,7 +17,29 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
     options.JsonSerializerOptions.MaxDepth = 32; // Gerekirse ayarlayın
-}); 
+});
+
+// IConfiguration'ı ekliyoruz
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+// JWT Authentication ayarları
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"]
+    };
+});
 
 builder.Services.AddDbContext<ToDoAppContext>(options=> options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
@@ -20,6 +47,8 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ITasksRepository, TasksRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ITasksService, TasksService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
