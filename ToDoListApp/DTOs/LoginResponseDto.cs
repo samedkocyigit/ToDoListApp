d428662e90b91bb6d5f5bbe647794f1dce85d7d6
samedkocyigit <abdulsamed.kcygt@gmail.com>
2024-10-06 17:57:08 +0300

    AutoMapper has been launched and Dto models has been created

---
+++ ToDoListApp/DTOs/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoListApp.DTOs
+{
+    public class LoginResponseDto
+    {
+        public int userId { get; set; }
+        public string token { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ ToDoListApp/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using ToDoListApp.DTOs;
+using ToDoListApp.Models;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CreateTaskDto, Tasks>();  
+        CreateMap<Tasks, TaskDto>();    
+    }
+}
